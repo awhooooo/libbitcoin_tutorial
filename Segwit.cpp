12bd2069b95acd52a8d4f0c11821f2c66c3f64b8
awhooooo <161196005+awhooooo@users.noreply.github.com>
2024-05-02 00:22:39 +0900

    Update Segwit.cpp

--- Segwit.cpp
+++ Segwit.cpp
@@ -26,17 +26,17 @@ std::string P2WSH(void)
     decode_hash(prev_txid1, "259e72177f337180074b002cb138f2f066ebd531e4c1f7ea8e6b8a474e2583ea");
 
     uint32_t input_index1 = 0;
-	  output_point vin1(prev_txid1, input_index1);
+    output_point vin1(prev_txid1, input_index1);
 
     //make Input
-  	input input1 = input();
-  	input1.set_previous_output(vin1);
-  	input1.set_sequence(0xfffffffe);
+    input input1 = input();
+    input1.set_previous_output(vin1);
+    input1.set_sequence(0xfffffffe);
 
     uint64_t output_value1;
-	  decode_base10(output_value1, "0.00999800", 8);
+    decode_base10(output_value1, "0.00999800", 8);
     script output_script1 = script().to_pay_key_hash_pattern(to_address1.hash());
-	  output output1(output_value1, output_script1);
+    output output1(output_value1, output_script1);
 
     // Get current time as a time_point
     auto now = std::chrono::system_clock::now();
@@ -46,29 +46,29 @@ std::string P2WSH(void)
     chain::transaction tx = chain::transaction();
     tx.set_version(2);
     tx.set_locktime(unix_time - 50000);
-  	tx.inputs().push_back(input1);
-  	tx.outputs().push_back(output1); 
+    tx.inputs().push_back(input1);
+    tx.outputs().push_back(output1); 
   
-  	endorsement sig1; 
-  	if (witness_script.create_endorsement(sig1, privateKey1.secret(), witness_script, tx, 0u, sighash_algorithm::all, script_version::zero, 1000000))
-  	{
-  		std::cout << "Signature: " << std::endl;
-  		std::cout << encode_base16(sig1) << "\n" << std::endl; 
-  	}
+    endorsement sig1; 
+    if (witness_script.create_endorsement(sig1, privateKey1.secret(), witness_script, tx, 0u, sighash_algorithm::all, script_version::zero, 1000000))
+    {
+	std::cout << "Signature: " << std::endl;
+	std::cout << encode_base16(sig1) << "\n" << std::endl; 
+    }
 
     //make Sig Script
-  	data_stack witness1; 
-  	witness1.push_back(to_chunk(sig1));
-  	witness1.push_back(to_chunk(pubKey1));
+    data_stack witness1; 
+    witness1.push_back(to_chunk(sig1));
+    witness1.push_back(to_chunk(pubKey1));
     witness1.push_back(to_chunk(witness_script.to_data(0)));
-  	witness txinwitness1(witness1);
-  	std::cout << txinwitness1.to_string() << "\n" << std:: endl;
+    witness txinwitness1(witness1);
+    std::cout << txinwitness1.to_string() << "\n" << std:: endl;
 
     // Make Signed TX
-	  tx.inputs()[0].set_witness(txinwitness1);
+    tx.inputs()[0].set_witness(txinwitness1);
     std::cout << "Raw Transaction: " << std::endl;
 
-	  std::cout << encode_base16(tx.to_data(true, true)) << std::endl;
+    std::cout << encode_base16(tx.to_data(true, true)) << std::endl;
 
     return encode_base16(tx.to_data(true, true));
 }
@@ -89,47 +89,47 @@ std::string P2WPKH(void)
     decode_hash(prev_txid1, "c34b7ecf6a53f30e699219660deb69abc385a68b725a7dabdb0503f17c79016d");
 
     uint32_t input_index1 = 0;
-	  output_point vin1(prev_txid1, input_index1);
+    output_point vin1(prev_txid1, input_index1);
 
     //make Input
-  	input input1 = input();
-  	input1.set_previous_output(vin1);
-  	input1.set_sequence(0xffffffff);
+    input input1 = input();
+    input1.set_previous_output(vin1);
+    input1.set_sequence(0xffffffff);
 
     uint64_t output_value1;
-	  decode_base10(output_value1, "0.00999800", 8);
+    decode_base10(output_value1, "0.00999800", 8);
     script output_script1 = script().to_pay_key_hash_pattern(to_address1.hash());
-	  output output1(output_value1, output_script1);
+    output output1(output_value1, output_script1);
 
     chain::transaction tx = chain::transaction();
     tx.set_version(2);
     tx.set_locktime(0);
-  	tx.inputs().push_back(input1);
-  	tx.outputs().push_back(output1); 
+    tx.inputs().push_back(input1);
+    tx.outputs().push_back(output1); 
 
     script p2wpkh_script_code = script::to_pay_key_hash_pattern(KeyHash);
     std::cout << p2wpkh_script_code.to_string(0) << std::endl;
 
-  	endorsement sig1; 
-      // script::create_endorsement(sig1, privateKey1.secret(), p2wpkh_script_code, tx, 0u, sighash_algorithm::all, script_version::zero, 1000000);
-  	if (witness_script.create_endorsement(sig1, privateKey1.secret(), p2wpkh_script_code, tx, 0u, sighash_algorithm::all, script_version::zero, 1000000))
-  	{
-  		std::cout << "Signature: " << std::endl;
-  		std::cout << encode_base16(sig1) << "\n" << std::endl; 
-  	}
+    endorsement sig1; 
+    // script::create_endorsement(sig1, privateKey1.secret(), p2wpkh_script_code, tx, 0u, sighash_algorithm::all, script_version::zero, 1000000);
+    if (witness_script.create_endorsement(sig1, privateKey1.secret(), p2wpkh_script_code, tx, 0u, sighash_algorithm::all, script_version::zero, 1000000))
+    {
+  	std::cout << "Signature: " << std::endl;
+  	std::cout << encode_base16(sig1) << "\n" << std::endl; 
+    }
 
     //make Sig Script
-  	data_stack witness1; 
-  	witness1.push_back(sig1);
-  	witness1.push_back(pubKey1);
-  	witness txinwitness1(witness1);
-  	std::cout << txinwitness1.to_string() << "\n" << std:: endl;
+    data_stack witness1; 
+    witness1.push_back(sig1);
+    witness1.push_back(pubKey1);
+    witness txinwitness1(witness1);
+    std::cout << txinwitness1.to_string() << "\n" << std:: endl;
 
     // Make Signed TX
-	  tx.inputs()[0].set_witness(txinwitness1);
+    tx.inputs()[0].set_witness(txinwitness1);
     std::cout << "Raw Transaction: " << std::endl;
 
-	  std::cout << encode_base16(tx.to_data(true, true)) << std::endl;
+    std::cout << encode_base16(tx.to_data(true, true)) << std::endl;
 
     return encode_base16(tx.to_data(true, true));
 }
