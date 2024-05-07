4a97cb84760c3fcf5265430e00c57e8d36ef57c4
awhooooo <161196005+awhooooo@users.noreply.github.com>
2024-05-07 16:31:50 +0900

    Update BTCMobick_Explorer_main.cpp

--- BTCMobick_ElectrumX/BTCMobick_Explorer_main.cpp
+++ BTCMobick_ElectrumX/BTCMobick_Explorer_main.cpp
@@ -6,12 +6,12 @@ int main()
 {
     Explorer_API explorer = Explorer_API();
     std::cout << explorer.get_utxo_set().dump(4) << std::endl;
-    // std::cout << explorer.get_address("1FeexV6bAHb8ybZjqQMjJrcCrHGW9sb6uF").dump(4) << std::endl;
-    // std::cout << explorer.get_next_block().dump(4) << std::endl;
+    std::cout << explorer.get_address("1FeexV6bAHb8ybZjqQMjJrcCrHGW9sb6uF").dump(4) << std::endl;
+    std::cout << explorer.get_next_block().dump(4) << std::endl;
     std::cout << explorer.get_unconfirmed_tx().dump(4) << std::endl;
     std::cout << explorer.get_transaction("163b2f4d7b0bb08fab9dcad8a0fb3c44daec328f3e9025b93a98a19ce0296fc4").dump(4) << std::endl;
-    // std::cout << explorer.get_block(556759).dump(4) << std::endl;
-    // std::cout << explorer.get_block_header(111194).dump(4) << std::endl;
+    std::cout << explorer.get_block(556759).dump(4) << std::endl;
+    std::cout << explorer.get_block_header(111194).dump(4) << std::endl;
     std::cout << explorer.get_height().dump(4) << std::endl;
     return 0;
 }
