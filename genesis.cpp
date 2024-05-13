326d0603141a31cd64bbf63a1aa7c74c043de498
awhooooo <161196005+awhooooo@users.noreply.github.com>
2024-05-13 11:20:52 +0900

    Update genesis.cpp

--- genesis.cpp
+++ genesis.cpp
@@ -8,13 +8,13 @@ using namespace bc;
 
 int main()
 {
-    // Extracting Satoshi's words from genesis block.
-    const auto block = bc::chain::block::genesis_mainnet();
-    const auto& coinbase = block.transactions().front();
-    const auto& input = coinbase.inputs().front();
-    BITCOIN_ASSERT_MSG(input.script().size() > 2u, "unexpected genesis");
-    const auto headline = input.script()[2].data();
-    std::string message(headline.begin(), headline.end());
-    std::cout << message << std::endl;
-    return EXIT_SUCCESS;
+        // Extracting Satoshi's words from genesis block.
+        const auto block = bc::chain::block::genesis_mainnet();
+        const auto& coinbase = block.transactions().front();
+        const auto& input = coinbase.inputs().front();
+        BITCOIN_ASSERT_MSG(input.script().size() > 2u, "unexpected genesis");
+        const auto headline = input.script()[2].data();
+        std::string message(headline.begin(), headline.end());
+        std::cout << message << std::endl;
+        return EXIT_SUCCESS;
 }
