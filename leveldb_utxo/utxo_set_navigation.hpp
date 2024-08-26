53d8eb69538854f571b9a125285ad8c64fc8e578
awhooooo <161196005+awhooooo@users.noreply.github.com>
2024-08-26 15:33:20 +0900

    Update and rename utxo_set_navigation.hpp to leveldb_utxo/utxo_set_navigation.hpp

--- leveldb_utxo/utxo_set_navigation.hpp
+++ leveldb_utxo/utxo_set_navigation.hpp
@@ -16,6 +16,7 @@
 // Constants
 const uint8_t COIN = 67;
 const std::vector<uint8_t> OBFUSCATE_KEY = {0x0e, 0x00, 'o', 'b', 'f', 'u', 's', 'c', 'a', 't', 'e', '_', 'k', 'e', 'y'};
+const int BATCH_SIZE = 1000;
 
 class UTXO_DB_READER
 {
@@ -33,6 +34,7 @@ class UTXO_DB_READER
         void get_obfuscate_key();
         void dump_chainstate_csv(const std::string& output_file);
 
-        // calculates the entire amount of coin recorded below the parameter height
-        uint64_t add_utxo_amount(const uint64_t block_height);
+        void check();
+        uint64_t process_batch_amount(std::vector<std::pair<std::string, std::vector<uint8_t>>>& batch, const std::vector<uint8_t>& obf_key, const uint64_t block_height);
+        uint64_t add_utxo_amount(const unsigned char start_byte, const unsigned char end_byte, const uint64_t block_height);
 };
